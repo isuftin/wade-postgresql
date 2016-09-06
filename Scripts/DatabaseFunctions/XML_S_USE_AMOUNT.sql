6740e9ce49adb2fd1b6ba229f5419dbf27e71964
zubayedrakib <zubayed_51@hotmail.com>
2016-09-06 13:02:43 -0600

    XML_S_USE_AMOUNT.sql

---
+++ Scripts/DatabaseFunctions/XML_S_USE_AMOUNT.sql
@@ -0,0 +1,60 @@
+﻿-- Function: "WADE_R"."XML_S_USE_AMOUNT"(text, text, text, numeric, numeric)
+
+-- DROP FUNCTION "WADE_R"."XML_S_USE_AMOUNT"(text, text, text, numeric, numeric);
+
+CREATE OR REPLACE FUNCTION "WADE_R"."XML_S_USE_AMOUNT"(
+    IN orgid text,
+    IN reportid text,
+    IN reportunitid text,
+    IN seqno numeric,
+    IN useid numeric,
+    OUT tmp_output xml)
+  RETURNS xml AS
+$BODY$
+
+BEGIN
+
+tmp_output:=(SELECT STRING_AGG
+	(XMLELEMENT
+		(name "WC:WaterUseAmount",
+			(SELECT XMLFOREST
+				("AMOUNT" AS "WC:AmountNumber",
+				"CONSUMPTIVE_INDICATOR" AS "WC:ConsumptiveIndicator")
+			),
+			(SELECT CASE WHEN C."METHOD_ID" IS NOT NULL THEN XMLELEMENT
+				(name "WC:Method",
+					(SELECT XMLFOREST
+						(C."METHOD_CONTEXT" AS "WC:MethodContext",
+						C."METHOD_NAME" AS "WC:MethodName")
+					)
+				)END
+			),
+			XMLELEMENT
+				(name "WC:TimeFrame",
+					(SELECT XMLFOREST
+						("START_DATE" AS "WC:TimeFrameStartName", 
+						"END_DATE" AS "WC:TimeFrameEndName")
+					)
+				)
+		)::text,''
+	)
+
+	FROM  
+
+	"WADE"."S_USE_AMOUNT" A LEFT OUTER JOIN "WADE"."METHODS" C ON (A."METHOD_ID"=C."METHOD_ID")
+	WHERE "ORGANIZATION_ID"=orgid AND "REPORT_ID"=reportid AND 
+	"REPORT_UNIT_ID"=reportunitid AND "SUMMARY_SEQ"=seqno);
+
+	RETURN;
+
+		
+
+END
+
+
+
+  $BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 1000;
+ALTER FUNCTION "WADE_R"."XML_S_USE_AMOUNT"(text, text, text, numeric, numeric)
+  OWNER TO "WADE";

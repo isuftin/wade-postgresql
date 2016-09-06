2223355569c9b2bb5e5d5d7c8479d9c8c0020942
zubayedrakib <zubayed_51@hotmail.com>
2016-09-06 12:09:23 -0600

    LU_FRESH_SALINE_INDICATOR.sql

---
+++ Scripts/DatabaseTables/LU_FRESH_SALINE_INDICATOR.sql
@@ -0,0 +1,41 @@
+﻿-- Table: "WADE"."LU_FRESH_SALINE_INDICATOR"
+
+-- DROP TABLE "WADE"."LU_FRESH_SALINE_INDICATOR";
+
+CREATE TABLE "WADE"."LU_FRESH_SALINE_INDICATOR"
+(
+  "LU_SEQ_NO" numeric(18,0) NOT NULL, -- A unique identifier for the Look-up value.
+  "CONTEXT" character varying(10) NOT NULL, -- Context for the lookk-up value
+  "VALUE" character varying(10) NOT NULL, -- Look up value.
+  "DESCRIPTION" character varying(255), -- Description of the look-up value.
+  "STATE" character(2), -- State that the Look-up value applies to.
+  "LAST_CHANGE_DATE" date, -- Last change date for the look-up.
+  "RETIRED_FLAG" character(1),
+  CONSTRAINT "PK_LU_FRESH_SALINE_INDICATOR" PRIMARY KEY ("LU_SEQ_NO"),
+  CONSTRAINT "UK_LU_FRESH_SALINE_INDICATOR-CONTEXT_VALUE" UNIQUE ("CONTEXT", "VALUE")
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "WADE"."LU_FRESH_SALINE_INDICATOR"
+  OWNER TO "WADE";
+COMMENT ON TABLE "WADE"."LU_FRESH_SALINE_INDICATOR"
+  IS 'Look up table for Fresh Saline Indicator.';
+COMMENT ON COLUMN "WADE"."LU_FRESH_SALINE_INDICATOR"."LU_SEQ_NO" IS 'A unique identifier for the Look-up value.';
+COMMENT ON COLUMN "WADE"."LU_FRESH_SALINE_INDICATOR"."CONTEXT" IS 'Context for the lookk-up value';
+COMMENT ON COLUMN "WADE"."LU_FRESH_SALINE_INDICATOR"."VALUE" IS 'Look up value.';
+COMMENT ON COLUMN "WADE"."LU_FRESH_SALINE_INDICATOR"."DESCRIPTION" IS 'Description of the look-up value.';
+COMMENT ON COLUMN "WADE"."LU_FRESH_SALINE_INDICATOR"."STATE" IS 'State that the Look-up value applies to.';
+COMMENT ON COLUMN "WADE"."LU_FRESH_SALINE_INDICATOR"."LAST_CHANGE_DATE" IS 'Last change date for the look-up.';
+
+
+-- Trigger: TG_LU_FRESH_SALINE_INDICATOR_CHANGE on "WADE"."LU_FRESH_SALINE_INDICATOR"
+
+-- DROP TRIGGER "TG_LU_FRESH_SALINE_INDICATOR_CHANGE" ON "WADE"."LU_FRESH_SALINE_INDICATOR";
+
+CREATE TRIGGER "TG_LU_FRESH_SALINE_INDICATOR_CHANGE"
+  BEFORE INSERT OR UPDATE
+  ON "WADE"."LU_FRESH_SALINE_INDICATOR"
+  FOR EACH ROW
+  EXECUTE PROCEDURE "WADE"."UPDATE_CHANGE_DATE"();
+
